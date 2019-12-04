d822563a861acd36d93d0fd5d56410c3dfe5f270
Neil Dobson <ndobson@barnardos.org.au>
2019-12-04 21:52:02 +1100

    Update packages

---
+++ src/Publish.ps1
@@ -0,0 +1,3 @@
+dotnet pack ./AutoInc/AutoInc.csproj -c Release
+dotnet pack ./AutoInc.Neo4j/AutoInc.Neo4j.csproj -c Release
+dotnet pack ./AutoInc.Neo4j.Signed/AutoInc.Neo4j.Signed.csproj -c Release
